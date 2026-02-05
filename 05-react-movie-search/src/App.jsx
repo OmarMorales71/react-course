e075f913533716558d0ef36ce5c69ca4121a9a2d
Omar Morales Ortega <omar.ortega@oracle.com>
2026-02-05 10:26:58 +0100

    Movie Search done

--- 05-react-movie-search/src/App.jsx
+++ 05-react-movie-search/src/App.jsx
@@ -3,22 +3,35 @@ import './App.css'
 import { useMovies } from './hooks/useMovies';
 import { Movies } from './components/Movies'
 import { useSearch } from './hooks/useSearch';
-import { useRef } from 'react';
-
+import { useState,useCallback } from 'react';
+import debounce from 'just-debounce-it'
 
 function App() {
-  const {search, updateSearch, error} = useSearch()
-  const { movies, getMovies, loading, errorMovies } = useMovies({search})
+  const [sort, setSort] = useState(false)
+  const { search, updateSearch, error } = useSearch()
+  const { movies, getMovies, loading, errorMovies } = useMovies({ search, sort })
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debouncedGetMovies = useCallback(
+    debounce((search) => {
+    console.log('search', search)
+    getMovies(search)
+  }, 2000), [getMovies])
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    getMovies()
+    getMovies(search)
+  }
+
+  const handleSort = () => {
+    setSort(!sort)
   }
 
   const handleChange = (event) => {
     const newQuery = event.target.value
     if (newQuery.startsWith(' ')) return
     updateSearch(newQuery)
+    debouncedGetMovies(newQuery)
   }
   return (
     <div className='page'>
@@ -26,13 +39,14 @@ function App() {
         <h1>Movie Search Engine</h1>
         <form className='form' onSubmit={handleSubmit}>
           <input style={{ border: '1px solid transparent', borderColor: error ? 'red' : 'transparent' }} onChange={handleChange} value={search} placeholder='Avengers, Harry Potter, The Matrix...' />
+          <input type='checkbox' onChange={handleSort} checked={sort} />
           <button type='submit'>Search</button>
         </form>
         {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
 
       <main>
-        {loading ? <p>Cargando...</p>:<Movies movies={movies} />}
+        {loading ? <p>Cargando...</p> : <Movies movies={movies} />}
         {errorMovies && <p style={{ color: 'red' }}>{errorMovies}</p>}
       </main>
     </div>
--- 05-react-movie-search/src/hooks/useMovies.js
+++ 05-react-movie-search/src/hooks/useMovies.js
@@ -1,15 +1,17 @@
 
 import { useState } from 'react'
 import { searchMovies } from '../services/movies'
-import { useRef } from 'react'
+import { useRef, useMemo } from 'react'
+import { useCallback } from 'react'
 
-export function useMovies({ search }) {
+export function useMovies({ search, sort }) {
     const [movies, setMovies] = useState([])
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
     const previousSearch = useRef(search)
 
-    const getMovies = async () => {
+    const getMovies = useCallback(async (search) => {
+        console.log("getMovies")
         if(previousSearch.current === search) return
         try {
             setLoading(true)
@@ -22,7 +24,17 @@ export function useMovies({ search }) {
         }finally{
             setLoading(false)
         }
-    }
+    }, [])
+    // const getSortedMovies = () => {
+    //     console.log("getSortedMovies")
+    //     return sort ? [...movies].sort((a,b)=> a.title.localeCompare(b.title)) : movies
 
-    return { movies, getMovies, loading, errorMovies: error }
+    // }
+
+    const sortedMovies= useMemo(() => {
+        console.log("getSortedMovies")
+        return sort ? [...movies].sort((a,b)=> a.title.localeCompare(b.title)) : movies
+    }, [sort, movies])
+
+    return { movies: sortedMovies, getMovies, loading, errorMovies: error }
 }
